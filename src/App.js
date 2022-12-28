e501ec37478b89c360c270a01564ba5d4847b036
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2022-12-28 23:26:55 +0900

    ダミーデータをreduxで取得して表示

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { useSelector } from 'react-redux';
 import './App.css';
 
 function App() {
+  const postList = useSelector((state) => state.posts.value);
+  console.log(postList);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>React-Redux掲示板</h1>
+      </div>
+       <div className='addPost'>
+        <input type="text" placeholder='お名前' />
+        <input type="text" placeholder='投稿内容' />
+        <button>投稿</button>
+        <hr />
+
+        <div className='displayPosts'>
+          {postList.map((post) => (
+            <div key={post.id} className="post">
+              <h1 className='postName'>{post.name}</h1>
+              <h1 className='postContent'>{post.content}</h1>
+              <button>削除</button>
+            </div>
+          ))}
+        </div>
+       </div>
     </div>
   );
 }
